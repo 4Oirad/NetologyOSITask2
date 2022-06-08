e9c2742dcd67861499dfb99f9df9aec9f61dcaf3
Bair Arshaev <ars-bair@yandex.ru>
2022-06-08 17:46:30 +0300

    Edit code 2

--- src/Client.java
+++ src/Client.java
@@ -6,16 +6,35 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         String host = "localhost";
-        String message = "Mister";
-        int port = 8090;
+        String message1 = "Hello Server";
+        int port = 8099;
+        Scanner scanner = new Scanner(System.in);
+
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new
                      PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new
                      InputStreamReader(clientSocket.getInputStream()))) {
-            out.println(message);
-            String resp = in.readLine();
-            System.out.println(resp);
+            out.println(message1);
+            System.out.println(message1);
+
+            String resp1 = in.readLine();
+            System.out.println(resp1);
+            String message2 = scanner.nextLine();
+            out.println(message2);
+
+            String resp2 = in.readLine();
+            System.out.println(resp2);
+            String message3 = scanner.nextLine();
+            out.println(message3);
+
+            String resp3 = in.readLine();
+            System.out.println(resp3);
+            String message4 = scanner.nextLine();
+            out.println(message4);
+
+            String resp4 = in.readLine();
+            System.out.println(resp4);
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -9,12 +9,30 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8090);
+        ServerSocket serverSocket = new ServerSocket(8099);
         Socket clientSocket = serverSocket.accept();
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         System.out.println("New connection accepted");
+        final String hello = in.readLine();
+
+        out.println("Hi. Write your name?");
         final String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+        out.println("What is your surname?");
+        final String surname = in.readLine();
+
+        out.println("Are you child? (yes/no)");
+        final String yes_no= in.readLine();
+
+        switch (yes_no) {
+            case "yes" :
+                out.println(String.format("Welcome to the kids area, %s %s! Let's play!", name, surname));
+                break;
+            case "no" :
+                out.println(String.format("Welcome to the adult zone, %s %s! Have a good rest, or a good working day!", name, surname));
+                break;
+            default:
+        }
     }
 }
