0c389025a7834c86b5183a8ad85ab96e12e7011e
Bair Arshaev <ars-bair@yandex.ru>
2022-06-15 00:12:26 +0300

    Edit code 2

--- src/Server.java
+++ src/Server.java
@@ -9,30 +9,31 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8099);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println("New connection accepted");
-        final String hello = in.readLine();
+        try (ServerSocket serverSocket = new ServerSocket(8099);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println("New connection accepted");
+            final String hello = in.readLine();
 
-        out.println("Hi. Write your name?");
-        final String name = in.readLine();
+            out.println("Hi. Write your name?");
+            final String name = in.readLine();
 
-        out.println("What is your surname?");
-        final String surname = in.readLine();
+            out.println("What is your surname?");
+            final String surname = in.readLine();
 
-        out.println("Are you child? (yes/no)");
-        final String yes_no= in.readLine();
+            out.println("Are you child? (yes/no)");
+            final String yes_no= in.readLine();
 
-        switch (yes_no) {
-            case "yes" :
-                out.println(String.format("Welcome to the kids area, %s %s! Let's play!", name, surname));
-                break;
-            case "no" :
-                out.println(String.format("Welcome to the adult zone, %s %s! Have a good rest, or a good working day!", name, surname));
-                break;
-            default:
+            switch (yes_no) {
+                case "yes" :
+                    out.println(String.format("Welcome to the kids area, %s %s! Let's play!", name, surname));
+                    break;
+                case "no" :
+                    out.println(String.format("Welcome to the adult zone, %s %s! Have a good rest, or a good working day!", name, surname));
+                    break;
+                default:
+            }
         }
     }
 }
